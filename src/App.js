21f9f3957ef7758a60192fc70d260c9bc831dc0a
vitu6569 <victoralmeida6569@gmail.com>
2024-07-13 15:04:26 -0300

    First uptdate :tada:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import illustration from './assets/images/illustration-article.svg'
+import './style.css'
 
-function App() {
+function Card() {
+  
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="card"
+    onMouseEnter={() => setIsHovered(true)}
+    onMouseLeave={() => setIsHovered(false)}
+    style={{ transform: isHovered ? 'scale(1.1)' : 'scale(1)', boxShadow: isHovered ? '15px 15px 0 2px' : ' 7px 7px 0px 2px',}}
+    >
+      <img id='illustration-article' src={illustration} alt="illustration-article"/>
+      <div id='title'>
+          <div id='learning'>Learning</div>
+          <div id='date'>Published 21 Dec 2023</div>
+      </div>  
+      <div id="description">
+          <div id="titleDescription">
+              <a href="https://www.frontendmentor.io/challenges/blog-preview-card-ckPaj01IcS/hub">HTML & CSS foundations</a>
+          </div>
+          <div id="textDescription">These languages are the backbone of every website, defining structure, content, and presentation.</div>
+      </div>
+      <div id="profile">
+          <div id="imageProfile"></div>
+          <div id="nameProfile">Greg Hooper</div>
+      </div>
+    
     </div>
+    
   );
 }
 
-export default App;
+export default Card;
